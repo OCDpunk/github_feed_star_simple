ade91d30b7e4f9efa66b3f69fc10474db56461d2
OCDpunk <admin@ocdpunk.com>
2020-02-27 02:23:41 +0800

    修改输出内容，添加emoji更加明显😁 && 修正用户名导致的读取失败

--- star.php
+++ star.php
@@ -5,6 +5,8 @@
  * Date: 2019-03-14
  * Time: 20:34
  */
+
+
 function result($code, $msg, $data)
 {
     return json_encode(['code' => $code, 'msg' => $msg, 'data' => $data]);
@@ -28,23 +30,23 @@ function curl_get($url, $data, $token, $type = 'GET')
 }
 
 
-function main($token)
+function main($token, $userName = 'ocdpunk')
 {
     $start = microtime(true);
-    $feed = json_decode(curl_get('https://api.github.com/users/ocdpunk/received_events', ['per_page' => 100], $token), true);
+    $feed = json_decode(curl_get('https://api.github.com/users/' . $userName . '/received_events', ['per_page' => 100], $token), true);
 
 //如果没有获取到动态
     if (in_array(substr($feed['code'], 0, 1), ['4', '5'])) {
-        echo result(500, '获取动态发生错误', $feed['data']);
+        exit(date('Y-m-d H:i:s') . "获取动态发生错误\n");
     }
-    $num = 0;
-    $false = 0;
-    $already = 0;
-    $zero = 0;
+    $num = 0;//数量
+    $false = 0;//关注错误
+    $already = 0;//已关注
+    $zero = 0;//关注是否为零（可能为刚创建）
     foreach (json_decode($feed['data'], true) as $k => $v) {
         $is_star = curl_get('https://api.github.com/user/starred/' . $v['repo']['name'], [], $token);
         if (json_decode($is_star, true)['code'] == 204) {
-            echo '已关注' . $v['repo']['name'] . "\n";
+            echo '项目：' . $v['repo']['name'] . "，已关注☑️\n";
             $already++;
             continue;
         }
@@ -52,7 +54,7 @@ function main($token)
         $count_star_user = curl_get('https://api.github.com/repos/' . $v['repo']['name'] . '/stargazers', [], $token);
         $star_numser = json_decode(json_decode($count_star_user, true)['data'], true);
         if (count($star_numser) == 0) {
-            echo "关注人数为0不关注".$v['repo']['name'] . "\n";
+            echo "项目：" . $v['repo']['name'] . "关注人数为零暂不关注❗️\n";
             $zero++;
             continue;
         }
@@ -60,18 +62,16 @@ function main($token)
         $do_star = curl_get('https://api.github.com/user/starred/' . $v['repo']['name'], [], $token, 'PUT');
         $do_star_http_code = json_decode($do_star, true)['code'];
         if ($do_star_http_code != 204) {
-            echo '关注' . $v['repo']['name'] . "失败\n";
+            echo '项目：' . $v['repo']['name'] . "关注失败❎\n";
             $false++;
             continue;
         }
-        echo '关注' . $v['repo']['name'] . "成功\n";
+        echo '项目：' . $v['repo']['name'] . "关注成功⭐️\n";
         $num++;
     }
     $end = microtime(true);
-    $time = round(($end - $start), 3);
-    echo "完成所有收录,花费时间 $time 秒，检查" .count(json_decode($feed['data'], true)) ."，项目共star $num 项目，关注0的 $zero 个项目，关注失败 $false 个项目。\n";
+    $time = round(($end - $start), 0);
+    echo date('Y-m-d H:i:s') . ",执行完成,花费时间 $time 秒，检查" . count(json_decode($feed['data'], true)) . "个项目，已关注 $already 个☑️，新关注 $num 个⭐，关注为0的 $zero 个❗，关注失败 $false 个❎。\n";
 }
 
-$token = $argv[1];
-
-main($token);
+main($argv[1], $userName = 'ocdpunk');
